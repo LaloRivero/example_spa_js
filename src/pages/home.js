f2cf9d916d19235e659a701d0fafc7a3b895f843
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-10-06 08:57:29 -0600

    Templates

---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+// Template home base
+
+const Home = () =>{
+    const view = `
+    <div class = "Characters">
+        <article class="Character item">
+            <a href='#/1/">
+                <img src="img" alt="img name">
+                <h3>Name</h3>
+            </a>
+        </article>
+    </div>`
+    return view
+}
+
+export default Home
---
+++ src/templates/header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+    const view = `
+        <div class="Header-main">
+            <div class="Header-logo">
+                <h1>
+                    <a href='/'> Web Services </a>
+                </h1>
+            </div>
+            <div class="Header-nav">
+                <a href="#/about/>
+                    About
+                </a>
+            </div>
+        </div>
+    `
+}
